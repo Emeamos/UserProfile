f678d623280f2ca012a511b81b4c851371000649
Emeamos <emediongamos.engr@gmail.com>
2022-11-01 04:09:26 +0100

    initial commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        // GET: api/<UserController>
+        [HttpGet]
+        public UserProfile Get()
+        {
+            return new UserProfile()
+            {
+                SlackUsername = "Emediong",
+                Backend = true,
+                Age = 100,
+                Bio = "Aspiring .NET developer"
+            };
+           
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ UserProfile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public class UserProfile
+    { 
+        public string SlackUsername { get; set; }
+        public bool Backend { get; set; }
+        public int Age { get; set; }
+        public string Bio { get; set; }
+    }
+}
